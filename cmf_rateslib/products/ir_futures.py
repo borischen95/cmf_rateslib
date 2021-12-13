91361356df6584374085302ef01f2e147335c775
bchenkuraev <boris_chen@mail.ru>
2021-12-13 23:34:04 +0300

    add convexity, make changes in pv

--- cmf_rateslib/products/ir_futures.py
+++ cmf_rateslib/products/ir_futures.py
@@ -4,11 +4,11 @@ Created on Mon Dec 13 01:59:44 2021
 
 @author: borischen
 """
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 class IR_Futures(object):
-    def __init__(self, expiry, tenor, size):
+    def __init__(self, expiry, tenor, size, kind = 'ED'):
         
         self._expiry = expiry
         self._size = size
@@ -19,32 +19,54 @@ class IR_Futures(object):
                 tenor = [int(word) for word in tenor.split(tenor[-1]) if word.isdigit()][0]*dic[tenor[-1]]
             else:
                 tenor = dic[tenor[-1]]
+                
+        self._type = kind
         self._tenor = tenor
         
         
-    def pv(self, curve, asof = 0):
-        result = 100 - 100*curve.fwd_rate(self._expiry, self._tenor)
+    def pv(self, curve = None, rate = None, asof = 0):
+        if (curve==None) and (rate==None):
+            print("Need argument rate or curve")
+            return None
+        
+        if curve == None:
+            rate = np.round(rate,4)
+            result = 100 - 100*rate
+            duration = ((100 - 100*rate-0.01)-(100 - 100*rate+0.01))/(2*0.01)
+        elif rate == None:
+            rate = np.round((1/curve.df(self._expiry)-1)/self._expiry, 4)
+            
+            result = 100 - 100*rate
+            duration = ((100 - 100*rate-0.01)-(100 - 100*rate+0.01))/(2*0.01)
+        self._pv = result
+        self._duration = duration
         return result
     
-    def get_cashflows(self, curve, asof = 0):
+    def get_cashflows(self, curve, asof = 0, trade_date = None):
         
+        
+        if trade_date == None:
+            trade_date = self._expiry
+            
         cashflow = []
         dv01 = self.dv01()
+        trade_rate = np.round((1/curve.df(trade_date)-1)/self._expiry, 4)
         i = asof
         k = 1/360
-        while i<self._expiry:
+        while i<trade_date:
             if i == asof:
-                cashflow.append(-self.pv(curve, asof))
+                cashflow.append(-self._pv)
             else:
-                cashflow.append(100*(curve.fwd_rate(i, self._tenor)-(curve.fwd_rate(i-k, self._tenor)))*dv01) 
+                cashflow.append(0) 
             i = i+k
-                                 
+        cashflow.append((self._pv-(100 - 100*trade_rate))*dv01)             
+            
         return cashflow
     
-    def cashflows_plot(self, curve, asof = 0):
+    def cashflows_plot(self, curve, asof = 0, trade_date = None):
         
         plt.figure(figsize = (15,7))
-        flows = self.get_cashflows(curve, asof)       
+        flows = self.get_cashflows(curve, asof, trade_date)       
         plt.plot(flows)
         plt.title('Cash Flows')
         plt.show()
@@ -54,3 +76,36 @@ class IR_Futures(object):
         dv = self._size*self._tenor*0.01**2
         
         return dv
+    
+    def duration(self):
+        
+        return self._duration
+    
+    def convexity(self, valuation_date, volatility, mean_reversion):
+       
+        """ Calculation of the convexity adjustment between FRAs and interest
+        rate futures using the Hull-White model as described in technical note
+        in link below:
+        http://www-2.rotman.utoronto.ca/~hull/TechnicalNotes/TechnicalNote1.pdf
+        NOTE THIS DOES NOT APPEAR TO AGREE WITH BLOOMBERG!! INVESTIGATE.
+        """
+
+        a = mean_reversion
+        t0 = 0.0
+        t1 = (self._expiry-2/252 - valuation_date) / 360
+        t2 = (self._expiry - valuation_date) / 360
+        # Hull White model for short rate dr = (theta(t)-ar) dt + sigma * dz
+        # This reduces to Ho-Lee when a = 0 so to avoid divergences I provide
+        # this numnerical limit
+        if abs(a) > 1e-10:
+
+            bt1t2 = (1.0 - np.exp(-a * (t2 - t1))) / a
+            bt0t1 = (1.0 - np.exp(-a * (t1 - t0))) / a
+            w = 1.0 - np.exp(-2.0 * a * t1)
+            term = bt1t2 * w + 2.0 * a * (bt0t1**2)
+            c = bt1t2 * (volatility**2) * term / (t2 - t1) / 4.0 / a
+
+        else:
+            c = t1 * t2 * (volatility**2) / 2.0
+
+        return c
